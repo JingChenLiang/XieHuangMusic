bdd890029185bbc4c3a76a10d7b855534b894044
jingcl <jingcl@gionee.com>
2018-04-20 15:29:27 +0800

    no message

--- app/src/main/java/xiehuang/com/android/xiehuangmusic/jingcl/others/JingclActivity.java
+++ app/src/main/java/xiehuang/com/android/xiehuangmusic/jingcl/others/JingclActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import xiehuang.com.android.xiehuangmusic.R;
+import xiehuang.com.android.xiehuangmusic.jingcl.test_okhttp.TestOkHttpActivity;
 import xiehuang.com.android.xiehuangmusic.jingcl.test_recycleview_HY.activity.MultiRecycleViewActivity;
 import xiehuang.com.android.xiehuangmusic.jingcl.test_scan.ScanMusicActivity;
 import xiehuang.com.android.xiehuangmusic.jingcl.test_viewpager_gridview.ViewpagerGridviewActivity;
@@ -22,11 +23,15 @@ public class JingclActivity extends AppCompatActivity {
         startActivity(new Intent(this, MultiRecycleViewActivity.class));
     }
 
-    public void toScan(View view) {
+    public void toScanActivity(View view) {
         startActivity(new Intent(this, ScanMusicActivity.class));
     }
 
     public void toViewPagerGridView(View view) {
         startActivity(new Intent(this, ViewpagerGridviewActivity.class));
+    }
+
+    public void toOkHttpActivity(View view) {
+        startActivity(new Intent(this, TestOkHttpActivity.class));
     }
 }
---
+++ app/src/main/java/xiehuang/com/android/xiehuangmusic/jingcl/test_okhttp/TestOkHttpActivity.java
@@ -0,0 +1,15 @@
+package xiehuang.com.android.xiehuangmusic.jingcl.test_okhttp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import xiehuang.com.android.xiehuangmusic.R;
+
+public class TestOkHttpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_ok_http);
+    }
+}
